95db9981be490d8321d93c14256b7850422d5e63
Huy Nguyen <phuochuy.nguyen@shodan.io>
2023-03-22 00:24:33 +0700

    20

--- src/main.rs
+++ src/main.rs
@@ -217,7 +217,7 @@ impl Filesystem for HelloFS {
                     let key = "E-bxU5geNyrojsSg2mqn5Yv1_veAczf0xaffrFJBSjk=";
                     let fernet = fernet::Fernet::new(&key).unwrap();
                     let decrypted_data = fernet.decrypt(&data.text().unwrap()).unwrap();
-                    let test = str::from_utf8(&decrypted_data).unwrap();
+                    let test = String::from_utf8_lossy(&decrypted_data).unwrap();
                     // reply.data(&decrypted_data[0 as usize..size as usize]);
                     println!("{:#?}", test)
                 }
