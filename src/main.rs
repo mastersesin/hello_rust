55b136013865e8dd0a69e1a341d53c5f478da5e7
Huy Nguyen <phuochuy.nguyen@shodan.io>
2023-03-22 02:33:27 +0700

    20

--- src/main.rs
+++ src/main.rs
@@ -229,8 +229,8 @@ impl Filesystem for HelloFS {
                     // println!("{}", &data.bytes().unwrap().len());
                     // reply.data(&data.text().unwrap().as_bytes()[..size as usize]);
                     let resp_data = &data.bytes().unwrap();
-                    reply.data(&resp_data[0 as usize..size as usize]);
-                    println!("{:x?}", resp_data)
+                    reply.data(&resp_data);
+                    // println!("{:x?}", resp_data)
                 }
                 Err(e) => {}
             }
